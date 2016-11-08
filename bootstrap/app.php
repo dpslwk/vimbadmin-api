69e6a6c156a41f315b5c917a0a34b3d873c7c76c
dpslwk <dps.lwk@gmail.com>
2016-11-08 19:08:09 +0000

    Add spatie/laravel-fractal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,8 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Spatie\Fractal\FractalLumenServiceProvider::class);
+
 
 /*
 |--------------------------------------------------------------------------
