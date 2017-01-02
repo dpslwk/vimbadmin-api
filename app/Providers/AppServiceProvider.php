3582cc5e03aab9361dacf84708e8f2ef98317bf0
dpslwk <dps.lwk@gmail.com>
2017-01-02 02:28:53 +0000

    Add custom ‘null’ validation rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+/**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        app('validator')->extend('null', function ($attribute, $value, $parameters, $validator) {
+            return is_null($value);
+        }, 'The :attribute must be null.');
+    }
+
+
     /**
      * Register any application services.
      *
